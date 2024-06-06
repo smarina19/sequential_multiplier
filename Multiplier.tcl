8620ddde37ab854ce725ab4fdfb94f02b5f12a11
Sofia Marina <smarina@princeton.edu>
2024-06-06 14:40:45 -0400

    first tests

--- Multiplier.tcl
+++ Multiplier.tcl
@@ -1,11 +1,11 @@
-// productDone is high 7-11 clock cycles after start is high
+# productDone is high 7-11 clock cycles after start is high
 assert {start |-> ##[7:11] productDone};
 
-// if both nonnegative, product is nonnegative
+# if both nonnegative, product is nonnegative
 assert {multiplier[3]==0 && multiplicand[3]==0 && productDone -> product[7]==0}
 
-// if multiplier positive and multiplicand negative, product is negative
+# if multiplier positive and multiplicand negative, product is negative
 assert {multiplier[3] == 0 && multiplier != 0 && multiplicand[3]==1}
 
-// if both negative, product is nonnegative
+# if both negative, product is nonnegative
 assert {multiplier[3]==1 && multiplicand[3]==1 && productDone -> product[7]==0}
