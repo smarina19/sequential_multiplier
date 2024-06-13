9272686e5b143cb0eea67369a3671aa187161cb1
Abiola Bolaji <ab2113@princeton.edu>
2024-06-13 16:55:51 -0400

    updated tester for communitive and associative

--- MultiplierTester.tcl
+++ MultiplierTester.tcl
@@ -6,7 +6,7 @@ clock clk
 reset rst
 
 # keep multiplicand constant to check for information leak of secret (multiplier)
-assume {multiplicandOne == multiplicandTwo}
+#assume {multiplicandOne == multiplicandTwo}
 
 # check if there is a timing attack possible that leaks secret
 assert {timingLeakDone -> !timingLeak}
