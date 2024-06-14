ea426eaddfe506ffb579cf25de866652a5f5c316
Abiola Bolaji <ab2113@princeton.edu>
2024-06-14 14:33:46 -0400

    fds

--- Multiplier.v
+++ Multiplier.v
@@ -5,7 +5,7 @@
 `include "MultiplierControl.v"
 `include "MultiplierDatapath.v"
 
-module Multiplier #(parameter WIDTH = 4)(
+module Multiplier #(parameter WIDTH = 1024)(
 	input   clk,
 	input   rst,
     input   start,
