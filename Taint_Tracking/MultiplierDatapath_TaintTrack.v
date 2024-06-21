3dee1ec1582182ee8f8e9ca52b91ec1f87e7871e
Abiola Bolaji <ab2113@princeton.edu>
2024-06-21 11:30:10 -0400

    fd

--- Taint_Tracking/MultiplierDatapath_TaintTrack.v
+++ Taint_Tracking/MultiplierDatapath_TaintTrack.v
@@ -46,30 +46,24 @@ always @( posedge clk) begin
     // init registers
     if (mdld) begin
         multiplicandReg <= multiplicand << WIDTH;
-        multiplicandReg_t <= multiplicand_t << WIDTH | {WIDTH{mdld_t}};
-    end
-    else begin
-        multiplicandReg_t <= multiplicandReg_t | {WIDTH{mdld_t}};
+        multiplicandReg_t <= multiplicand_t << WIDTH;
     end
 
     if (mrld) begin
         multiplierReg <= multiplier;
-        multiplierReg_t <= multiplier_t | {WIDTH{mrld_t}};
-    end
-    else begin
-        multiplierReg_t <= multiplierReg_t | {WIDTH{mrld_t}};
+        multiplierReg_t <= multiplier_t;
     end
 
     if (rsclear) begin
         runningSumReg <= 0;
-        runningSumReg_t <= 0 | {WIDTH{rsclear_t}} | {WIDTH{rsload_t}} | {WIDTH{rsshr_t}};
+        runningSumReg_t <= 0;
     end
 
     // load running sum
-    else if (rsload) begin
+    if (rsload) begin
         runningSumReg <= multiplicandReg + runningSumReg; 
-        // addition logic - I don't know how to implement w/o using this weird loop logic
-        runningSumReg_t <= multiplicandReg_t | runningSumReg_t | {WIDTH{rsclear_t}} | {WIDTH{rsload_t}} | {WIDTH{rsshr_t}};
+        // addition logic - I don't know how to implement w/o using block assignment :(
+        runningSumReg_t <= multiplicandReg_t | runningSumReg_t;
         carry = 0;
         for (i = 0; i < WIDTH - 1; i = i + 1) begin
             if (((multiplicandReg[i] & runningSumReg[i]) | 
@@ -78,7 +72,7 @@ always @( posedge clk) begin
             begin
                   carry = 1;
                   if (multiplicandReg_t[i] | runningSumReg_t[i]) begin
-                    runningSumReg_t[i + 1] <= 1; // Propagate taint to the next bit if carry occurs in addition, should it be one or runningSumReg_t[i]
+                    runningSumReg_t[i + 1] <= 1; // Propagate taint to the next bit if carry occurs in addition, (should it be one or runningSumReg_t[i])
                   end
             end
             else begin
@@ -87,16 +81,15 @@ always @( posedge clk) begin
         end
     end
 
-    else if (rsshr) begin
+    if (rsshr) begin
         runningSumReg <= runningSumReg >>> 1; 
-        runningSumReg_t <= runningSumReg_t | {WIDTH{rsclear_t}} | {WIDTH{rsload_t}} | {WIDTH{rsshr_t}};
     end
 
-    else begin 
-        runningSumReg_t <= runningSumReg_t | {WIDTH{rsclear_t}} | {WIDTH{rsload_t}} | {WIDTH{rsshr_t}};
-    end
-
-    end 
+    // taint logic depends on control bits
+    multiplicandReg_t <= multiplicandReg_t | {WIDTH{mdld_t}};
+    multiplierReg_t <= multiplierReg_t | {WIDTH{mrld_t}};
+    runningSumReg_t <= runningSumReg_t | {WIDTH{rsclear_t}} | {WIDTH{rsload_t}} | {WIDTH{rsshr_t}};
+end 
     assign product = runningSumReg;
     assign product_t = runningSumReg_t;
 endmodule
