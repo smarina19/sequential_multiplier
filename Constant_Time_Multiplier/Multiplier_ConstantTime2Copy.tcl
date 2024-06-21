1cf6ed9e1d4e0a2cbe07dcde41247b32e8007fb4
Abiola Bolaji <ab2113@princeton.edu>
2024-06-21 15:31:09 -0400

    added constant 2 copy tester and script

---
+++ Constant_Time_Multiplier/Multiplier_ConstantTime2Copy.tcl
@@ -0,0 +1,9 @@
+analyze -sv Multiplier_Constant2CopyTester.v
+
+elaborate -top Multiplier_ConstantTime
+
+clock clk
+reset rst
+
+# check if there is a timing attack possible that leaks secret
+assert {multiplicandOne == multiplicandTwo && timingLeakDone -> !timingLeak}
