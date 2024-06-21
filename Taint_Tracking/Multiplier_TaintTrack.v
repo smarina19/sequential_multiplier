27d5eb40c435ed6f93ca9aec33c3449c580d2637
Abiola Bolaji <ab2113@princeton.edu>
2024-06-21 11:58:38 -0400

    testing

--- Taint_Tracking/Multiplier_TaintTrack.v
+++ Taint_Tracking/Multiplier_TaintTrack.v
@@ -5,7 +5,7 @@
 `include "MultiplierControl_TaintTrack.v"
 `include "MultiplierDatapath_TaintTrack.v"
 
-module Multiplier_TaintTrack #(parameter WIDTH = 8)(
+module Multiplier_TaintTrack #(parameter WIDTH = 16)(
 	input   clk,
 	input   rst,
     input   start,
