31f014bd28eb2e557e9b795142a91d9c1feb4e59
unknown <oam2323@hotmail.com>
2024-06-24 14:41:32 -0400

    self composition tests

---
+++ Constant_Time_Multiplier/Multiplier_StateBranchTester.tcl
@@ -0,0 +1,13 @@
+analyze -sv Multiplier_StateBranchTester.v
+
+elaborate -top Multiplier_StateBranchTester
+
+clock clk
+reset rst
+
+# check if there is a timing attack possible that leaks secret
+assert {multiplicandOne == multiplicandTwo && timingLeakDone -> !timingLeak}
+
+# Set the time limit to 1 hour (3600 seconds)
+set_prove_time_limit 3600
+prove -all
