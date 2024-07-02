cbcb2aefe00b1e64d6f33e3a4f905ca8df168da4
Abiola Bolaji <ab2113@princeton.edu>
2024-07-01 21:36:34 -0400

    d

--- Constant_Time_Multiplier/Multiplier_StateBranchTester.v
+++ Constant_Time_Multiplier/Multiplier_StateBranchTester.v
@@ -4,7 +4,7 @@
 
 `include "Multiplier_StateBranch.v"
 
-module Multiplier_StateBranchTester #(parameter WIDTH = 64)(
+module Multiplier_StateBranchTester #(parameter WIDTH = 128)(
     input   clk,
 	input   rst,
     input   start,
