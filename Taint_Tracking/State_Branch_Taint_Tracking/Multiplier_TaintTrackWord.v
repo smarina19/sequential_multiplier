ed0fc0c57a474675c9437c02c7dc2dd605873e78
Abiola Bolaji <ab2113@princeton.edu>
2024-07-02 15:56:11 -0400

    d

--- Taint_Tracking/State_Branch_Taint_Tracking/Multiplier_TaintTrackWord.v
+++ Taint_Tracking/State_Branch_Taint_Tracking/Multiplier_TaintTrackWord.v
@@ -5,7 +5,7 @@
 `include "MultiplierControl_TaintTrackWord.v"
 `include "../MultiplierDatapath_TaintTrack1Bit.v"
 
-module Multiplier_TaintTrackWord #(parameter WIDTH = 32)(
+module Multiplier_TaintTrackWord #(parameter WIDTH = 64)(
 	input   clk,
 	input   rst,
     input   start,
