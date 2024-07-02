0388e4924131d161770f764dc61a1fff665c78fe
Abiola Bolaji <ab2113@princeton.edu>
2024-07-01 21:12:27 -0400

    s

--- Constant_Time_Multiplier/Multiplier_StateBranch.v
+++ Constant_Time_Multiplier/Multiplier_StateBranch.v
@@ -5,7 +5,7 @@
 `include "MultiplierControl_StateBranch.v"
 `include "../MultiplierDatapath.v"
 
-module Multiplier_StateBranch #(parameter WIDTH = 32)(
+module Multiplier_StateBranch #(parameter WIDTH = 64)(
 	input   clk,
 	input   rst,
     input   start,
