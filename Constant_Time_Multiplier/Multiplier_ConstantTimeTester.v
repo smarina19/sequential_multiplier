a785f83baba732652fcfd3ba86aeeed81366589d
Abiola Bolaji <ab2113@princeton.edu>
2024-07-01 21:25:04 -0400

    s

--- Constant_Time_Multiplier/Multiplier_ConstantTimeTester.v
+++ Constant_Time_Multiplier/Multiplier_ConstantTimeTester.v
@@ -4,7 +4,7 @@
 
 `include "Multiplier_ConstantTime.v"
 
-module Multiplier_ConstantTimeTester #(parameter WIDTH = 32)(
+module Multiplier_ConstantTimeTester #(parameter WIDTH = 64)(
     input   clk,
 	input   rst,
     input   start,
