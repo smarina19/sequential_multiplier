2f32273536103d7f732392d3a4637edccb34e179
Abiola Bolaji <ab2113@princeton.edu>
2024-07-02 15:49:21 -0400

    s

--- Taint_Tracking/State_Branch_Taint_Tracking/Multiplier_TaintTrackBitwise.v
+++ Taint_Tracking/State_Branch_Taint_Tracking/Multiplier_TaintTrackBitwise.v
@@ -5,7 +5,7 @@
 `include "MultiplierControl_TaintTrackBitwise.v"
 `include "../MultiplierDatapath_TaintTrack.v"
 
-module Multiplier_TaintTrackBitwise #(parameter WIDTH = 8)(
+module Multiplier_TaintTrackBitwise #(parameter WIDTH = 16)(
 	input   clk,
 	input   rst,
     input   start,
