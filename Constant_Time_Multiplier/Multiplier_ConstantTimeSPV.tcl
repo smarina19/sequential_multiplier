0a47f6200a0e232c1ae6dd02ff31e28320a2eb00
Abiola Bolaji <ab2113@princeton.edu>
2024-07-19 11:49:00 -0400

    d

--- Constant_Time_Multiplier/Multiplier_ConstantTimeSPV.tcl
+++ Constant_Time_Multiplier/Multiplier_ConstantTimeSPV.tcl
@@ -8,4 +8,8 @@ reset rst
 # check_spv -create -from multiplier -to {product}
 check_spv -create -from multiplier -to {productDone}
 
+# Set the time limit to 1 hour (3600 seconds)
+set_prove_time_limit 3600
+set_engine_mode Tri
 prove -all
+
