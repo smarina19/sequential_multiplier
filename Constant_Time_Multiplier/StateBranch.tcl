928fc46298cf9ec54ae4c5cc9a2350f7cd20dc0a
Sofia Marina <smarina@princeton.edu>
2025-02-03 11:45:34 -0500

    fall updates

--- Constant_Time_Multiplier/StateBranch.tcl
+++ Constant_Time_Multiplier/StateBranch.tcl
@@ -5,9 +5,7 @@ elaborate -top Multiplier_StateBranch
 clock clk
 reset rst
 
-# check_spv -create -from multiplier -to {product}
 check_spv -create -from multiplier -to {productDone}
 
 set_prove_time_limit 3600
-set_engine_mode Tri
 prove -all
